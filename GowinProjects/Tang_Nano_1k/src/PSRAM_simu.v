14fd11f6bf77d0673f3b98726dfd2f1f8420e4c0
Gytakemoto <gytakemoto@yahoo.com.br>
2024-04-04 11:43:01 -0300

    Functional step proccesses

--- GowinProjects/Tang_Nano_1k/src/PSRAM_simu.v
+++ GowinProjects/Tang_Nano_1k/src/PSRAM_simu.v
@@ -1,16 +1,18 @@
 //File created to simulate PSRAM module in a Icarus Verilog simulation environment
 
-//Clock generator *** simulation only
+
 `timescale 1ns/1ps
 
+//Clock generator *** simulation only
 module clk_gen (
 	input enable,
+
 	output reg clk
 );
+
+//Local parameters
 localparam PHASE = 0;							//Phase
 localparam DUTY = 50;							//Duty cycle
- 
-
 parameter real FREQ = 27;
 parameter real clk_pd = 1/(FREQ * 1e6) * 1e9; 			//Clock period in ns
 parameter real clk_on = DUTY/100.0 * clk_pd;  			//On state in ns depending on duty cycle
@@ -18,8 +20,11 @@ parameter real clk_off = (100 - DUTY)/100.0 * clk_pd; 	//Off state in ns dependi
 parameter real quarter = clk_pd/4;
 parameter real start_dly = quarter * PHASE/90;			//Delay depending on clock PHASE
 
-reg start_clk;
+//Variables
+reg start_clk;		//Control clock output depending on clock enable
 
+//Debug in terminal
+/*
 initial begin
 	$display("FREQ = %0d MHz", FREQ);
 	$display("PHASE = %0d deg", PHASE);
@@ -31,6 +36,7 @@ initial begin
 	$display("QUARTER = %0.3f ns", quarter);
 	$display("START_DELAY = %0.3f ns", start_dly);
 end
+*/
 
 initial begin
 	clk <= 0;
@@ -62,59 +68,152 @@ always @ (posedge start_clk) begin
 end
 endmodule
 
-module psram(
-	input clk,                   // memory clock <= 84 MHz
-  input startbu,               // start button to initialize PSRAM
-	inout wire [3:0] mem_sio,    // sio[0] pin 22, sio[1] pin 23, sio[2] pin 24, sio[3] pin 21
-	output wire mem_ce,        // pin 
-	input wire mem_clk,          // pin 
-	output reg [3:0] step
+module mem_driver(
+	input sys_clk,
+	input mem_clk,
+	input [7:0] command,
+	input [3:0] step,
+	input msg_sw,
+	input command_sw,
+	
+	output mem_ce,
+	output reg [3:0] mem_sio
 );
 
+//Reinitialization commands
+parameter [7:0] CMD_RSTEN = 8'h66;
+parameter [7:0] CMD_RST = 8'h99;
+
+reg [3:0] counter = 0;
+reg sendcommand = 0;
+reg [7:0] mem_command;
+
+initial begin
+	counter <= 0;
+end
+
+
+always @(posedge mem_clk) begin
+	//$display(psram.STEP_RSTEN);
+
+	if(msg_sw) begin
+		sendcommand <= 1;
+	end
+
+	if (sendcommand && command_sw) begin
+		$display("O valor do comando eh %b", command);
+		$display("agora o comando_sw eh",command_sw);
+		mem_sio[0] <= command[counter];
+		mem_sio[1] <= 1'bz;
+		counter <= counter + 1'd1;
+		$display("O comando para o contador %d eh %d", counter, command[counter]);
+		$display("o valor de sendcommand eh %d", sendcommand);
+		//$display(command[counter]);
+	end
+
+	if (counter == 3'd7) begin
+		sendcommand <= 0;
+		counter <= 0;
+	end
+end
+
+assign mem_ce = ~sendcommand;
+endmodule
+
+module psram(
+	input sys_clk,                  // system clock 27 MHz
+	input mem_clk,
+  	input startbu,              // start button to initialize PSRAM
+	
+	output mem_ce,        		// pin 
+	output reg [3:0] step,		//*** simulation only
+	output reg [15:0] timer,		//*** simulation only
+	output reg [7:0] command,	//*** simularion only
+
+	inout [3:0] mem_sio    	// sio[0] pin 22, sio[1] pin 23, sio[2] pin 24, sio[3] pin 21
+);
+
+//Local parameters
 localparam [2:0] STEP_DELAY = 0; //First state, wait for 150us
 localparam [2:0] STEP_RSTEN = 1; //Second state, RSTEN operation
 localparam [2:0] STEP_RST = 2;   //Third state, RST state
 
-//step = STEP_DELAY; 	//indicates the current operation
-								//0: 150us required delay
-								//1: Reset enable (RSTEN) step
-								//2: Reset (RST) step
-								//3: Idle normal operation state
+localparam [2:0] STEP_IDLE = 4;
 
-reg [15:0] timer = 0;
-reg [7:0] command;
-reg start = 0;				  	//start initialization
-reg msg_sw = 0;						//allows to send message
-reg sleepy_mem = 1; 			//indicates whether the PSRAM is initialized
-						  						//1: sleep mode
-						  						//0: active memory
+//Variables
+//reg [3:0] step = STEP_DELAY;  //indicates the current operation
+									//0: 150us required delay
+									//1: Reset enable (RSTEN) step
+									//2: Reset (RST) step
+									//3: Idle normal operation state
 
-initial begin 
+//reg [15:0] timer = 0;			//Counter
+//reg [7:0] command;				//SPI/QPI 8 bit command
+reg start = 0;				  	//Start initialization
+
+reg msg_sw = 0;					//Allows to send message
+									//0: No message being sent
+									//1: Message being sent
+
+assign command_sw = msg_sw;			//Indicates when command is being sent
+									//0: Command not being sent
+									//1: Command being sent
+
+wire [7:0] command_mem = command;
+
+
+mem_driver PSRAM_comms(
+	.sys_clk(sys_clk),
+	.mem_clk(mem_clk),
+	.command(command_mem),
+	.step(step),
+	.mem_sio(mem_sio),
+	.mem_ce(mem_ce),
+	.msg_sw(msg_sw),
+	.command_sw(command_sw)
+);
+
+initial begin
 	step <= STEP_DELAY;
+	timer <= 0;
+	msg_sw <= 0;
+	command <= 0;
 end
 
-always @(posedge clk) begin
+always @(posedge sys_clk) begin
 
-	if(startbu) start = 1;			//detect button pressed
-
-	if(sleepy_mem && start && !msg_sw) begin			//begin initialization if mem is sleepy, startbu was pressed and message isn't being sent
-		case(step)
-			STEP_DELAY: begin
-				if(timer[15:8] == 8'h32) begin 		// #32h = #50d. 50 * 256 (thus the 8-bit swap) = 12.800 clocks inputs. At 84Mhz, we have a t ~= 152 us.
-					step <= STEP_RSTEN;
-					timer <= 16'b0;
-					end
-					timer <= timer + 1;
-        end
-			/*STEP_RSTEN: begin
-				command <= mem_driver.CMD_RSTEN;
-			end
-			STEP_RST: begin
-				command <= CMD_RST;
-			end*/
-		endcase	
+	if(startbu) begin
+		 start = 1;			//Detect button pressed
 	end
 
-	if(msg_sw == 1) msg_sw <= 0; 					//if message was sent, disable it
+	if(start && step != STEP_IDLE) begin		//Begin initialization if mem isn't idle, startbu was pressed and message isn't being sent
+		case(step)
+			STEP_DELAY: begin
+				timer <= timer + 1;
+				if(timer[15:8] == 8'h10) begin 		// #32h = #50d. 50 * 256 (thus the 8-bit swap) = 12.800 clocks inputs. At 84Mhz, we have a t ~= 152 us.
+					step <= STEP_RSTEN;
+					timer <= 16'b0;					//Reset timer
+				end
+        	end
+			STEP_RSTEN: begin
+				//$display("entrou no step rsten");
+				command <= PSRAM_comms.CMD_RSTEN;
+				msg_sw <= 1;
+				if(mem_ce && msg_sw)	begin
+					step <= STEP_RST;
+					msg_sw <= 0;
+				end
+				$display("dentro do step, o comando eh %b", command);
+			end
+			STEP_RST: begin
+				msg_sw <= 1;
+				command <= PSRAM_comms.CMD_RST;
+				if(mem_ce && msg_sw)	begin
+					step <= STEP_IDLE;
+					msg_sw <= 0;
+				end
+			end
+		endcase	
+	end
 end
 endmodule
